40c93988e6b91d6ced4345a52b22147d3f49fbb4
Shaun Plummer <Shaunplummer247@googlemail.com>
2021-02-28 12:23:39 +0000

    refactor: run spotless and apply copyright
    Run the spotless gradle task specified in the challenge requirements to format code and add copyright

--- app/src/main/java/com/example/androiddevchallenge/ListViews.kt
+++ app/src/main/java/com/example/androiddevchallenge/ListViews.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.foundation.Image
@@ -20,7 +35,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
-
 
 @Preview("Puppy List")
 @Composable
@@ -59,7 +73,6 @@ fun petCard(dog: Dog) {
         modifier = Modifier
             .padding(4.dp)
             .clickable {
-
             },
         color = MaterialTheme.colors.surface,
         elevation = 4.dp,
@@ -85,6 +98,5 @@ fun petCard(dog: Dog) {
                 modifier = Modifier.fillMaxWidth()
             )
         }
-
     }
 }
--- app/src/main/java/com/example/androiddevchallenge/PetData.kt
+++ app/src/main/java/com/example/androiddevchallenge/PetData.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import kotlin.random.Random
@@ -12,7 +27,7 @@ object PetData {
 
     private val randomWeight: Int
         get() = Random.nextInt(10, 40)
-    
+
     /**
      * A list of dogs
      */
@@ -25,5 +40,4 @@ object PetData {
         Dog("Zuul", randomAge, R.raw.dog_006, "M", randomWeight),
         Dog("Vinz", randomAge, R.raw.dog_007, "F", randomWeight)
     )
-
 }
